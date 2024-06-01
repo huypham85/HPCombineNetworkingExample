36628bda045b2f964d641bc81b609d99a2a632e3
Huy Pham <huypham8501@gmail.com>
2024-06-01 11:50:50 +0700

    Initial Commit

---
+++ HPCombineNetworkingExample/HPCombineNetworkingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPCombineNetworkingExampleApp.swift
+//  HPCombineNetworkingExample
+//
+//  Created by Huy Pham on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct HPCombineNetworkingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
